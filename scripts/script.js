f3a87caba4646925f962c547b97a8137d5b56ea5
joaocunha <jvscunha04@gmail.com>
2023-10-11 21:24:03 -0300

    feat: add function navigationMembers

--- scripts/script.js
+++ scripts/script.js
@@ -4,5 +4,18 @@ const members = [
     { name: "Drax o destruidor", id: "Drax" },
     { name: "Mantis", id: "mantis" },
     { name: "Rocket Raccoon", id: "rocket" },
-    { name: "I am Groot", id: "groot" },
+    { name: "I am Groot", id: "groot" }
 ]
+
+let activeMembers = 0;
+
+const images = document.getElementById("images")
+
+function navigationMembers(direction) {
+    activeMembers += direction;
+
+    const member = members[activeMembers];
+    console.log(activeMembers, member);
+
+    images.style.transform = `translateY(${-100 * activeMembers}vh)`
+}
