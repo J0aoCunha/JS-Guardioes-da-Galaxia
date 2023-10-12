36435fa74dd7924a9730433daae55f005192eb1f
joaocunha <jvscunha04@gmail.com>
2023-10-11 21:13:33 -0300

    feat: create and style members

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const members = [
+    { name: "Peter Quill", id: "Peter" },
+    { name: "Gamora Titan", id: "gamora" },
+    { name: "Drax o destruidor", id: "Drax" },
+    { name: "Mantis", id: "mantis" },
+    { name: "Rocket Raccoon", id: "rocket" },
+    { name: "I am Groot", id: "groot" },
+]
